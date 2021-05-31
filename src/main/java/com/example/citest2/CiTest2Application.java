f3b963df574ddddcc066eff38842c52f49d99663
niewei <niewei@ebaolife.com>
2021-05-31 17:40:44 +0800

    Initial commit

---
+++ src/main/java/com/example/citest2/CiTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.citest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/citest2/CiTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
